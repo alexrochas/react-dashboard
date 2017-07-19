6231b859c73bfb6f3ce1fc6313f231f8e6559049
Alex Rocha <alex.rochas@yahoo.com.br>
2017-07-18 21:10:48 -0300

    feat: add widget

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Widget from '../components/Widget';
+import NumberDisplay from '../components/NumberDisplay';
 
 // Import widgets being used in this component
 
@@ -9,8 +11,9 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                {/* Add Widgets to display */}
-
+                <Widget heading="Open ticket total">
+                    <NumberDisplay max={9} value={5} />
+                </Widget>
             </div>
         );
     }
--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/Widget.css';
 
 // Import components
 import Loading from './Loading';
@@ -11,21 +12,45 @@ class Widget extends Component {
 
         // Create inline styles to make grid elements span multiple rows/columns
         this.spanStyles = {};
+        if (props.colspan !== 1) {
+            this.spanStyles.gridColumn
+                = `span ${props.colspan}`;
+        }
+        if (props.rowspan !== 1) {
+            this.spanStyles.gridRow
+                = `span ${props.rowspan}`;
+        }
     }
 
     render() {
         return (
-            <div className="Widget">
-                <p>This is a widget</p>
+            <div className="Widget" style={this.spanStyles}>
+                <div className="header">
+                    <h2>{this.props.heading}</h2>
+                    {this.props.loading ? <Loading/> : ""}
+                </div>
+                <div className="content">
+                    {this.props.children}
+                </div>
             </div>
         );
     }
 }
 
 // Provide default settings for when they aren't supplied
-
+Widget.defaultProps = {
+  heading: "Unnamed Widget",
+  colspan: 1,
+  rowspan: 1
+}
 
 // Enforce the type of props to send to this component
-
+Widget.propTypes = {
+  heading: React.PropTypes.string,
+  colspan: React.PropTypes.number,
+  rowspan: React.PropTypes.number,
+  children:
+    React.PropTypes.element.isRequired
+}
 
 export default Widget;
